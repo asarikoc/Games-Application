cee1919495aeeb8a584c336d6f4cfb677eecaf8a
asarikoc <abdulkadirsarikoc@hotmail.com>
2023-01-02 01:39:17 +0300

    feat: add swipe to delete from favorites

--- app/src/main/java/com/example/games/FavoritesFragment.kt
+++ app/src/main/java/com/example/games/FavoritesFragment.kt
@@ -1,17 +1,67 @@
 package com.example.games
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class FavoritesFragment : Fragment() {
+
+    lateinit var v: View
+
+    private lateinit var myrecyclerview: RecyclerView
+    private lateinit var listGames: ArrayList<Games>
+    lateinit var games : Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        v = inflater.inflate(R.layout.fragment_favorites, container, false)
+
+        myrecyclerview = v.findViewById(R.id.favorites_recyclerview)
+        myrecyclerview.layoutManager = LinearLayoutManager(activity)
+        myrecyclerview.setHasFixedSize(true)
+
+        listGames = ArrayList()
+        listGames.add(Games(R.drawable.gta5, "Grand Theft Auto V", "96", "Action, shooter"))
+        listGames.add(Games(R.drawable.portal2, "Portal 2", "95", "Action, puzzle"))
+
+        var adapter = RecyclerViewAdapter(listGames)
+        myrecyclerview.adapter = adapter
+
+        adapter.setOnItemClickListener(object: RecyclerViewAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+
+                val intent = Intent(activity,GamesActivity::class.java)
+                intent.putExtra("imageID",listGames[position].titleImage)
+                intent.putExtra("heading",listGames[position].heading)
+                intent.putExtra("metacritic",listGames[position].metacritic)
+                intent.putExtra("genre",listGames[position].genre)
+                intent.putExtra("games",games[position])
+                startActivity(intent)
+            }
+        })
+
+
+        val swipeToDeleteCallback = object : SwipeToDeleteCallback(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                listGames.removeAt(position)
+                myrecyclerview.adapter?.notifyItemRemoved(position)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+
+        itemTouchHelper.attachToRecyclerView(myrecyclerview)
+
+        return v
     }
 }
